1fab90def1e358fa6f277b71afa890ec27052870
Jay Park <jaypark.cs@gmail.com>
2018-04-09 12:31:23 -0400

    adding IAP POC and removing unwind segue

---
+++ AEEInAppPurchase/inappragedemo/IAPManager.swift
@@ -0,0 +1,14 @@
+//
+//  IAPManager.swift
+//  AEETestPurchase
+//
+//  Created by Jay Park on 3/30/18.
+//  Copyright © 2018 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+class IAPManager:NSObject {
+  
+}
